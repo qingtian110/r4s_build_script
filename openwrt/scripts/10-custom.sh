7186d750f740e24eb926deefe203c8bc957b2b3b
qingtian110 <87856890+qingtian110@users.noreply.github.com>
2025-12-17 18:34:11 +0800

    Implement custom script for package management
    Add custom script to clone repositories and update i18n.

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 # 自定义脚本
+
+# istore
+git clone https://github.com/sbwml/package_new_istore package/new/istore
+
+# autocore
+rm -rf package/system/autocore
+git clone https://github.com/qingtian110/autocore-arm package/system/autocore
+
+# i18n
+cat >> package/new/default-settings/i18n/default.zh_Hans.po <<"EOF"
+
+msgid "Contact Us"
+msgstr "交流讨论、固件定制、技术支持"
+
+EOF
